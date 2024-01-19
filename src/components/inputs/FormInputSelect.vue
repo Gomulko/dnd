965d10cbaed694d8166b3c9c2bd6d199fd1f2eb3
NeverDieDevil <michal.obszan@gmail.com>
2024-01-19 18:33:20 +0100

    select input is now written with ts

--- src/components/inputs/FormInputSelect.vue
+++ src/components/inputs/FormInputSelect.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-    <label :for="{ id }">Wybierz {{ id }}: </label>
+    <label :for="id">Wybierz {{ id }}: </label>
     <br />
-    <select :name="{ id }" :id="{ id }" v-model="selected">
-      <option v-for="value in values" :key="value">{{ value }}</option>
+    <select :name="id" :id="id" v-model="selected">
+      <option v-for="value in values" :key="(value as keyof typeof String)">
+        {{ value }}
+      </option>
     </select>
   </div>
 </template>
 
 <script setup lang="ts">
-  import { defineProps, ref } from "vue";
+  import { defineProps, ref, computed } from "vue";
   import { useMainStore } from "@/store";
 
   const props = defineProps({
@@ -22,9 +24,14 @@
       required: true,
     },
   });
+
   const store = useMainStore();
-  const selected = ref();
-  store.updateFormData(props.id as keyof typeof store.formData, selected);
+  const selected = computed({
+    get: () => store.formData[props.id as keyof typeof store.formData],
+    set: (newValue) => {
+      store.updateFormData(props.id as keyof typeof store.formData, newValue);
+    },
+  });
 </script>
 
 <style lang="scss" scoped></style>
